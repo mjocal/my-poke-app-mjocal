87d93ae0d63a584fa2aaf570df0e4bedce101a3f
Maria Jose Calderon <dra.mariacalderon@gmail.com>
2022-10-22 01:50:31 -0600

    pokedex added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "../styles/global.scss";
 import {
   Card,
@@ -17,99 +17,112 @@ import { Container } from "@mui/system";
 import { useFirebaseAuth } from "../contexts/FirebaseContext";
 import { Link, useNavigate } from "react-router-dom";
 import { Formik } from "formik";
-import * as Yup from "yup";
-
-const MIN_PASSWORD_CHARACTERS = 8;
 
 export default function Login() {
   const { login } = useFirebaseAuth();
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = async (email, password) => {
+  const handleFormSubmit = async (values) => {
     try {
-      await login(email, password);
+      await login(values.email, values.password);
+      navigate("/home");
     } catch (error) {
-      setError("Log in failed");
+      alert("failed login");
+      console.error(error);
+      setError("Login failed");
     }
   };
 
-  //termina forma antigua
-
-  // // version majo
-  //   async function handleSubmit(e) {
-  //     e.preventDefault();
-
-  //     try {
-  //       setError("");
-  //       setLoading(true);
-  //       await login(emailRef.current.value, passwordRef.current.value);
-  //       navigate("/home");
-  //     } catch {
-  //       setError("Log in failed");
-  //     }
-  //     setLoading(false);
-  //   }
-
   return (
     <main>
-      <Card sx={{ minWidth: 345, maxWidth: 500 }}>
-        <CardMedia
-          component="img"
-          height="140"
-          image={banner}
-          alt="pokemon banner"
-        />
-        <Container>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Welcome back!
-            </Typography>
-            {error && <Alert severity="error">{error}</Alert>}
-            <form onSubmit={handleSubmit}>
-              <FormControl variant="standard" fullWidth={true}>
-                <InputLabel htmlFor="email">Email</InputLabel>
-                <Input
-                  id="email"
-                  type="email"
-                  // inputRef={emailRef}
-                  // onChange={handleEmailChange}
-                  required
-                />
-              </FormControl>
-              <br />
-              <FormControl variant="standard" fullWidth={true}>
-                <InputLabel htmlFor="password">Password</InputLabel>
-                <Input
-                  id="password"
-                  type="password"
-                  // inputRef={passwordRef}
-                  // onChange={handlePasswordChange}
-                  required
-                />
-              </FormControl>
-              <Button
-                size="medium"
-                variant="contained"
-                type="submit"
-                style={{
-                  marginTop: "1rem",
-                }}
-              >
-                Log in
-              </Button>
-            </form>
-          </CardContent>
-          <CardActions>
-            <Typography variant="body2" color="text.secondary">
-              New to Pokemanazos ?
-            </Typography>
-            <Button component={Link} to="/signin" size="small">
-              Register here
-            </Button>
-          </CardActions>
-        </Container>
-      </Card>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleFormSubmit}
+        validateOnBlur
+        validateOnChange
+        validateOnMount
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          isValid,
+          isValidating,
+        }) => (
+          <Card sx={{ minWidth: 345, maxWidth: 500 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={banner}
+              alt="pokemon banner"
+            />
+            <Container>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Welcome back!
+                </Typography>
+                {error && <Alert severity="error">{error}</Alert>}
+                <form onSubmit={handleSubmit}>
+                  <FormControl variant="standard" fullWidth={true}>
+                    <InputLabel htmlFor="email">Email</InputLabel>
+                    <Input
+                      id="email"
+                      error={touched.email && !!errors.email}
+                      required
+                      label="Email"
+                      type="email"
+                      value={values.email}
+                      onChange={handleChange("email")}
+                      onBlur={handleBlur("email")}
+                    />
+                  </FormControl>
+                  <br />
+                  <FormControl variant="standard" fullWidth={true}>
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <Input
+                      id="password"
+                      error={touched.password && !!errors.password}
+                      required
+                      label="Password"
+                      type="password"
+                      value={values.password}
+                      onChange={handleChange("password")}
+                      onBlur={handleBlur("password")}
+                    />
+                  </FormControl>
+                  <Button
+                    disabled={!isValid}
+                    size="medium"
+                    variant="contained"
+                    type="submit"
+                    style={{
+                      marginTop: "1rem",
+                    }}
+                  >
+                    Log in
+                  </Button>
+                </form>
+              </CardContent>
+              <CardActions>
+                <Typography variant="body2" color="text.secondary">
+                  New to Pokemanazos ?
+                </Typography>
+                <Button component={Link} to="/signin" size="small">
+                  Register here
+                </Button>
+              </CardActions>
+            </Container>
+          </Card>
+        )}
+      </Formik>
     </main>
   );
 }
