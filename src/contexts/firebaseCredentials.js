71bd7f70648326e47b2c561d29a845e059db7fd4
Maria Jose Calderon <dra.mariacalderon@gmail.com>
2022-10-28 00:11:09 -0600

    WIP Poke Context (Windows)

--- src/contexts/firebaseCredentials.js
+++ src/contexts/firebaseCredentials.js
@@ -9,6 +9,14 @@ const app = initializeApp({
   messagingSenderId: "468188492269",
   appId: "1:468188492269:web:494c4a332323dafcc3f7e4",
   measurementId: "G-78ZXXZEJ2X",
+
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY ?? "",
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ?? "",
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ?? "",
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ?? "",
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ?? "",
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID ?? "",
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID ?? "",
 });
 
 export const auth = getAuth();
