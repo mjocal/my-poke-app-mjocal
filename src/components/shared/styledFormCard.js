10cf67672a5877fd0be0a8556c4bff53e4fa0a1f
Majo Calderon <dra.mariacalderon@gmail.com>
2023-02-23 00:37:17 -0600

    updated styled form cars width

--- src/components/shared/styledFormCard.js
+++ src/components/shared/styledFormCard.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Card } from "@mui/material";
 
 const StyledFormCard = styled(Card)`
-  max-width: 50vw;
+  max-width: 25vw;
 
   @media (max-width: 768px) {
     max-width: 80vw;
